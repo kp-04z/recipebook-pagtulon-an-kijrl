8555b029087afee5908a35e07320ad8abadc6031
kp-04z <kijrl.pagtulon-an@student.ateneo.edu>
2025-03-10 20:35:29 +0800

    Added ability for users to create accounts

--- ledger/views.py
+++ ledger/views.py
@@ -1,25 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import RecipeIngredient, Ingredient, Recipe
-
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import RecipeIngredient, Ingredient, Recipe
 
 class RecipeListView(ListView):
     model = Recipe
     template_name = 'ledger/recipes_list.html'
     
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'ledger/recipe_detail.html'
-
-#def recipes(request):
-#    recipes = Recipe.objects.all()
-#    ctx = { "recipes": recipes }
-#    return render(request, 'ledger/recipes_list.html', ctx)
-
-#def recipe_detail(request, pk):
-#    ctx = {'recipe': Ingredient.objects.filter(recipe__recipe__name=pk)}
-#    
-#    return render(request, 'ledger/recipe_detail.html', ctx)
+    redirect_field_name = '/accounts/login'
--- recipebook/settings.py
+++ recipebook/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ledger'
+    'ledger',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -121,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+LOGIN_REDIRECT_URL = '/recipes/list'
+LOGOUT_REDIRECT_URL = '/accounts/login'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
