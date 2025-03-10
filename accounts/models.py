fb02f6894197b7209ffb3aee37b073cfe7ebee16
kp-04z <kijrl.pagtulon-an@student.ateneo.edu>
2025-03-10 20:52:02 +0800

    Added appropriate max_length values for Profile fields in models.py

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    name = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(blank=True)
+    name = models.OneToOneField(User, on_delete=models.CASCADE, max_length=50)
+    bio = models.TextField(blank=True, max_length=255)
     
     
